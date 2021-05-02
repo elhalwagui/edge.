3372df6b49d33388c47090d5e68b0943497986aa
ELHALWAGUI\User <elhalwaguiahmedmohamed@gmail.com>
2021-05-02 17:01:05 +0200

    Prepare for deployment

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 
 const itemRouter = require('./routes/itemRoutes');
 
 module.exports = app;
+
+app.use(compression());
 // Configuring the url
 app.use('/api/v1/items', itemRouter);
